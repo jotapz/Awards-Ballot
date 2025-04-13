72f83816e95a6f6cdf037318fe099408e9c19fed
Julia Mattos <jxliamattos@gmail.com>
2025-04-13 16:38:40 -0300

    componentes template vers.1

--- src/components/TemplateVotacao/TemplateVotacao.jsx
+++ src/components/TemplateVotacao/TemplateVotacao.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const TemplateVotacao = () => {
     return (
-        <div className="flex items-center h-full w-full">
-            <div className="w-[716px] h-[550px] max-w-full max-h-full sm:w-[50%] sm:h-[80%] bg-[#E5D9B6] rounded-xl shadow-lg p-6 overflow-auto">
-                teste
+        <div className="w-[1300px] h-[715px] bg-[#E5D9B6] rounded-xl">
+            <div className="w-[355px] h-[485px] bg-[#E5D9B6] rounded-xl border-4 border-black ms-250">
+
             </div>
         </div>
     )
