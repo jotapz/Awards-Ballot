00f451b3a960a40eba6e272a548ee64b5dca9ce1
Julia Mattos <jxliamattos@gmail.com>
2025-04-27 20:52:46 -0300

    teste

--- src/components/TemplateVotacao/TemplateVotacao.jsx
+++ src/components/TemplateVotacao/TemplateVotacao.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const TemplateVotacao = () => {
     return (
-        <div className="w-[1300px] h-[715px] bg-[#E5D9B6] rounded-xl">
-            <div className="w-[355px] h-[485px] bg-[#E5D9B6] rounded-xl border-4 border-black ms-250">
+        <div className="w-[1300px] h-[80%] bg-[#E5D9B6] rounded-xl">
+            <div className="w-[355px] h-[350px] bg-[#E5D9B6] rounded-xl border-4 border-black ms-250">
 
             </div>
         </div>
