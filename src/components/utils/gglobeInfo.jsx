7c6bc3ba9d59624cddfb54dbfa993c3fa27424a4
João Pedro <jpedros1511@gmail.com>
2025-05-11 13:58:46 -0300

    feature: adicionando gglobeInfo json

---
+++ src/components/utils/gglobeInfo.jsx
@@ -0,0 +1,304 @@
+const categories = {
+    awardsName: "Grammy 2026",
+    categories: [
+      {
+        // MELHOR FILME DRAMA
+        category: "Best Motion Picture - Drama",
+        explanation: "Premia o melhor longa-metragem com narrativa dramática.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi The Brutalist (A24)'
+      },
+      {
+        // MELHOR FILME MUSICAL OU COMEDIA
+        category: "Best Motion Picture - Musical or Comedy",
+        explanation: "Reconhece o melhor longa-metragem com foco em comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Emilia Pérez (Netflix)'
+      },
+      {
+        // MELHOR ATRIZ EM FILME - DRAMA
+        category: "Best Performance by a Female Actor in a Motion Picture – Drama",
+        explanation: "Premia a melhor atuação feminina em papel dramático.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Fernanda Torres – Im Still Here'
+      },
+      {
+        // MELHOR ATOR EM FILME - DRAMA
+        category: "Best Performance by a Male Actor in a Motion Picture – Drama",
+        explanation: "Premia a melhor atuação masculina em papel dramático.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Adrien Brody – The Brutalist'
+      },
+      {
+        // MELHOR ATRIZ EM FILME - MUSICAL OU COMÉDIA
+        category: "Best Performance by a Female Actor in a Motion Picture – Musical or Comedy",
+        explanation: "Premia a melhor atuação feminina em filme de comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Demi Moore – The Substance'
+      },
+      {
+        // MELHOR ATOR EM FILME - MUSICAL OU COMÉDIA
+        category: "Best Performance by a Male Actor in a Motion Picture – Musical or Comedy",
+        explanation: "Premia a melhor atuação masculina em filme de comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Sebastian Stan – A Different Man'
+      },
+      {
+        // MELHOR ATRIZ COADJUVANTE EM FILME
+        category: "Best Performance by a Female Actor in a Supporting Role in Any Motion Picture",
+        explanation: "Premia a melhor atuação feminina em papel coadjuvante em qualquer gênero.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Zoe Saldaña – Emilia Pérez'
+      },
+      {
+        // MELHOR ATOR COADJUVANTE EM FILME
+        category: "Best Performance by a Male Actor in a Supporting Role in Any Motion Picture",
+        explanation: "Premia a melhor atuação masculina em papel coadjuvante em qualquer gênero.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Kieran Culkin – A Real Pain'
+      },
+      {
+        // MELHOR DIREÇÃO EM FILME
+        category: "Best Director – Motion Picture",
+        explanation: "Reconhece o melhor trabalho de direção em um longa-metragem.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Brady Corbet – The Brutalist'
+      },
+      {
+        // MELHOR ROTEIRO EM FILME
+        category: "Best Screenplay – Motion Picture",
+        explanation: "Premia o melhor roteiro original ou adaptado.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Jacques Audiard – Emilia Pérez'
+      },
+      {
+        // MELHOR TRILHA SONORA ORIGINAL
+        category: "Best Original Score – Motion Picture",
+        explanation: "Reconhece a melhor composição musical original para um filme.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Clément Ducol & Camille – Emilia Pérez'
+      },
+      {
+        // MELHOR CANÇÃO ORIGINAL
+        category: "Best Original Song – Motion Picture",
+        explanation: "Premia a melhor música composta especificamente para um filme.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi “El Mal” – Emilia Pérez'
+      },
+      {
+        // MELHOR FILME EM LINGUA ESTRANGEIRA
+        category: "Best Motion Picture – Non-English Language",
+        explanation: "Premia filmes produzidos fora dos EUA com idioma original diferente do inglês.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Emilia Pérez (França)'
+      },
+      {
+        // MELHOR FILME DE ANIMAÇÃOO
+        category: "Best Motion Picture – Animated",
+        explanation: "Premia o melhor longa-metragem de animação.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Flow'
+      },
+      {
+        // MELHOR SERIE DE TV - DRAMA
+        category: "Best Television Series – Drama",
+        explanation: "Premia a melhor série com narrativa dramática.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Shōgun'
+      },
+      {
+        // MELHOR SERIE DE TV - MUSICAL OU COMEDIA
+        category: "Best Television Series – Musical or Comedy",
+        explanation: "Premia a melhor série de comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Hacks'
+      },
+      {
+        // MELHOR ATRIZ EM SERIE DE TV - DRAMA
+        category: "Best Performance by a Female Actor in a Television Series – Drama",
+        explanation: "Premia a melhor atuação feminina em série dramática.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Anna Sawai – Shōgun'
+      },
+      {
+        // MELHOR ATOR EM SERIE DE TV - DRAMA
+        category: "Best Performance by a Male Actor in a Television Series – Drama",
+        explanation: "Premia a melhor atuação masculina em série dramática.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Hiroyuki Sanada – Shōgun'
+      },
+      {
+        // MELHOR ATRIZ EM SERIE DE TV - MUSICAL OU COMEDIA
+        category: "Best Performance by a Female Actor in a Television Series – Musical or Comedy",
+        explanation: "Premia a melhor atuação feminina em série de comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Jean Smart – Hacks'
+      },
+      {
+        // MELHOR ATOR EM SERIE DE TV - MUSICAL OU COMEDIA
+        category: "Best Performance by a Male Actor in a Television Series – Musical or Comedy",
+        explanation: "Premia a melhor atuação Masculina em série de comédia ou musical.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Jeremy Allen White – The Bear'
+      },
+      {
+        // MELHOR Minissérie, Série Antológica ou Filme para TV
+        category: "Best Television Limited Series, Anthology Series or Motion Picture Made for Television",
+        explanation: "Premia produções para TV com histórias fechadas.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Beef'
+      },
+      {
+        // MELHOR Atriz em Minissérie, Série Antológica ou Filme para TV
+        category: "Best Performance by a Female Actor in a Limited Series, Anthology Series or Motion Picture Made for Television",
+        explanation: "Premia a melhor atuação feminina nesse formato.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Ali Wong – Single Lady'
+      },
+      {
+        // MELHOR Ator em Minissérie, Série Antológica ou Filme para TV
+        category: "Best Performance by a Male Actor in a Limited Series, Anthology Series or Motion Picture Made for Television",
+        explanation: "Premia a melhor atuação masculina nesse formato.",
+        nominees: [
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" },
+          { name: "A", description: "A" }
+        ],
+        lastWinnerDescription: 'O último vencedor dessa catégoria foi Steven Yeun – Beef'
+      },
+    ]
+}
