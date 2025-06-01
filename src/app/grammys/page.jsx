416b9922b2f32f7e9bb0c383778e71102731fa6a
Eduarda Saleth <95479157+dudaomss@users.noreply.github.com>
2025-06-01 00:32:27 -0300

    feat: atualiza conteudo generico de votacoes

--- src/app/grammys/page.jsx
+++ src/app/grammys/page.jsx
@@ -1,10 +1,26 @@
+"use client"
+
 import TemplateVotacao from "@/components/TemplateVotacao/TemplateVotacao";
+import { categoriesData } from "@/components/utils/grammyInfo";
+import { useCategoryNavigation } from "@/components/utils/useCategoryNavigation";
 
 const Page = () => {
+    const categorias = categoriesData.categories;
+    const { index, category, next, prev } = useCategoryNavigation(categorias);
+
     return (
         <div className="text-white">
-        <TemplateVotacao/>
+            <TemplateVotacao
+                disabledNext={index === categorias.length - 1}
+                disabledPrevious={index === 0}
+                onClickNext={next}
+                onClickPrevious={prev}
+                categories={{
+                    ...category,
+                    awardsName: categoriesData.awardsName
+                }}
+            />
         </div>
     );
-    }
+}
 export default Page;
